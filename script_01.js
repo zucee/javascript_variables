fd417993470f885872754c63fcb8467aee073d80
zucee <kujon@gmx.net>
2021-05-18 14:24:16 +0200

    Erste Variablen, Übungen 21.05.18

--- script_01.js
+++ script_01.js
@@ -1,1 +1,53 @@
 /***** Variablen 01 *******/
+
+// Zeilenkommentar
+
+/**
+   Blockkommentar
+ */
+
+// Ausgabe
+//console.log("Hallo Welt");
+//console.log(firstName);
+
+// Deklaration + Wertzuweisung
+/*
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName);
+*/
+
+// Deklaration + Wertzuweisung II
+/*
+let firstName,familyName;
+firstName = prompt("Bitte Vornamen  eingeben");
+familyName = prompt("Bitte Namen eingeben");
+console.log("Hallo, " + firstName + " " + familyName +"!");
+*/
+
+// JS ist eine untypisierte Sprache! | untyped
+/*
+let test; // Deklaration
+test = "hi"; //string
+test = 2; //number
+test = true; //boolean
+console.log("Typ: " + typeof test);
+console.log("Inhalt: " + test);
+*/
+
+/***** Variablen 02 *****/
+
+//Deklaration
+let ageJohn, ageMark;
+let birthYearJohn, birthYearMark;
+
+// Wertzuweisung | Assignment
+let date = new Date();
+let year = date.getFullYear();
+//console.log(year);
+//console.log(date);
+ageJohn = 25; 
+ageMark = 30;
